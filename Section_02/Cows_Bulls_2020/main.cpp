f2147f239b7d2c64fb6b6644ad13a9a2d19ce416
Maja_Krzyzaniak <maja_krzyzaniak@outlook.com>
2020-01-08 19:36:24 +0100

    Lubię jak jest tak jak lubię

--- Section_02/Cows_Bulls_2020/main.cpp
+++ Section_02/Cows_Bulls_2020/main.cpp
@@ -3,27 +3,38 @@
 
 using namespace std;
 
-int main() {
-	// Introduce the game
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// the entry point for our application
+int main() 
+{
+	PrintIntro();
+
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+
+	cout << endl;
+	return 0;
+}
+
+// Introduce the game
+void PrintIntro() {
 	constexpr int WORLD_LENGHT = 5;
 	cout << "Welcome to Cows and Bulls, a raw word game.\n";
 	cout << "Can you guess the " << WORLD_LENGHT;
 	cout << " letter isogram I'm thinking of?\n";
 	cout << endl;
+	return;
+}
 
-	// get a guess from the player
+// get a guess from the player
+string GetGuessAndPrintBack() {
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	// repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
 
-	// get a guess from the player
-	cout << "Enter your guess: ";
-	getline(cin, Guess);
-	// repeat the guess back to them
+	// print the guess back to them
 	cout << "Your guess was: " << Guess << endl;
-
-	cout << endl;
-	return 0;
+	return Guess;
 }
