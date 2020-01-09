59bea9690291fdda671c327373cb8e0500532422
Maja_Krzyzaniak <maja_krzyzaniak@outlook.com>
2020-01-09 23:41:43 +0100

    lekcja 32

---
+++ Section_02/Cows_Bulls_2020/FCowBullGame.cpp
@@ -0,0 +1,26 @@
+#include "FCowBullGame.h"
+
+void FCowBullGame::Reset()
+{
+	return;
+}
+
+int FCowBullGame::GetMaxTries()
+{
+	return 0;
+}
+
+int FCowBullGame::GetCurrentTry()
+{
+	return 0;
+}
+
+bool FCowBullGame::IsGameWon()
+{
+	return false;
+}
+
+bool FCowBullGame::CheckGuessValidity(std::string)
+{
+	return false;
+}
